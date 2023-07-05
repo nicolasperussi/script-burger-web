8851802bb8be8b9954ac5c48caac61429cd45349
nicolasperussi <nicolasperussi12@hotmail.com>
2023-07-05 15:49:26 -0300

    fix(visualizer): removed all delivery instances from context

--- src/context/OrdersContext.tsx
+++ src/context/OrdersContext.tsx
@@ -1,16 +1,12 @@
 import React, { createContext, ReactNode, useEffect, useState } from "react";
 import { IOrder } from "../types/IOrder";
-import { IDelivery } from "../types/IDelivery";
 import socketIo from "socket.io-client";
 import { api } from "../services/api";
 
 type OrderContextType = {
   orders: IOrder[] | null | undefined;
-  deliveries: IDelivery[] | null | undefined;
   isFetchingOrders: boolean;
-  isFetchingDeliveries: boolean;
   handleSetOrders(orders: IOrder[]): void;
-  handleSetDeliveries(deliveries: IDelivery[]): void;
 };
 
 type ProductProviderProps = {
@@ -19,11 +15,8 @@ type ProductProviderProps = {
 
 export const OrderContext = createContext<OrderContextType>({
   orders: [],
-  deliveries: [],
   isFetchingOrders: true,
-  isFetchingDeliveries: true,
   handleSetOrders: (orders: IOrder[]) => {},
-  handleSetDeliveries: (deliveries: IDelivery[]) => {},
 });
 
 function OrderProvider({ children }: ProductProviderProps) {
@@ -33,24 +26,11 @@ function OrderProvider({ children }: ProductProviderProps) {
   function handleSetOrders(orders: IOrder[]) {
     setOrders(orders);
   }
-
-  const [deliveries, setDeliveries] = useState<IDelivery[]>([]);
-  const [isFetchingDeliveries, setIsFetchingDeliveries] = useState(true);
-
-  function handleSetDeliveries(deliveries: IDelivery[]) {
-    setDeliveries(deliveries);
-  }
-
   useEffect(() => {
     api
       .get("/order")
       .then((response) => setOrders(response.data))
       .finally(() => setIsFetchingOrders(false));
-
-    api
-      .get("/delivery")
-      .then((response) => setDeliveries(response.data))
-      .finally(() => setIsFetchingDeliveries(false));
   }, []);
 
   useEffect(() => {
@@ -67,11 +47,8 @@ function OrderProvider({ children }: ProductProviderProps) {
     <OrderContext.Provider
       value={{
         orders,
-        deliveries,
         isFetchingOrders,
-        isFetchingDeliveries,
         handleSetOrders,
-        handleSetDeliveries,
       }}
     >
       {children}
